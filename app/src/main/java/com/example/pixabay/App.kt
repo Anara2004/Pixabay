155f2ed86af9cab9d80bee403855ab7aa7f63c83
Anara2004 <anarabazarbaeva2004@gmail.com>
2022-10-09 17:32:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/pixabay/App.kt
@@ -0,0 +1,15 @@
+package com.example.pixabay
+
+import android.app.Application
+
+class App: Application() {
+
+    companion object {
+        lateinit var api: PixabayApi
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        api = RetrofitServices().api
+    }
+}
